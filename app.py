1180081907e0c857f5fe24d746ece81f5a2b5b90
mpcosta <markkcosta@gmail.com>
2024-01-25 03:07:09 -0100

    Implementing healthcheck and base metrics endpoint along with logging

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+import logging
 import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%dT%H:%M:%SZ')
+db_connection_count = 0
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global db_connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    db_connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -30,24 +36,54 @@ def index():
     connection.close()
     return render_template('index.html', posts=posts)
 
+# Define endpoint used for Health Check
+@app.route('/healthz')
+def status():
+    response = app.response_class(
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info('Requested /healthz endpoint successful')
+    return response
+
+# Define endpoint used for metrics related data
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    post_count = connection.execute('SELECT COUNT(*) FROM posts').fetchone()[0]
+    connection.close()
+    response = app.response_class(
+        response=json.dumps(
+            {"status": "success", "metrics_data": {"db_connection_count": db_connection_count, "post_count": post_count}}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info('Requested /metrics endpoint successful')
+    return response
+
 # Define how each individual article is rendered 
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        app.logger.warn('Non-existing Article has been accessed - returning 404 page')
+        return render_template('404.html'), 404
     else:
+      app.logger.info(f'Article: {post["title"]} successfully retrieved!')
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('Requested /about endpoint successful')
     return render_template('about.html')
 
 # Define the post creation functionality 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
+    app.logger.info('Requested /metrics endpoint successful')
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
@@ -61,6 +97,7 @@ def create():
             connection.commit()
             connection.close()
 
+            app.logger.info(f'News Article: {title} created successfully')
             return redirect(url_for('index'))
 
     return render_template('create.html')
